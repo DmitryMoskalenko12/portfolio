9e3f2988e9ef2c9da560520639f68c5f2b73de13
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-03-13 17:33:01 +0200

    fix: burder

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -4,9 +4,9 @@ const ToggleButton = ({ setOpen }) => {
     return (
         <button onClick={() => setOpen((prev) => !prev)}>
             <svg width='23' height='23' viewBox="0 0 23 23">
-                <motion.path variants={{closed: {d: 'M 2 2.5 L 20 2.5'}, open: {d: 'M 3 16.5 L 17 2.5'}}} strokeWidth='3' stroke='black' strokeLinecap="round"/>
+                <motion.path variants={{closed: {d: 'M 2 2.5 L 20 2.5'}, open: {d: 'M 3 16.5 L 17 2.5'}}} strokeWidth='3' d='M 2 2.5 L 20 2.5' stroke='black' strokeLinecap="round"/>
                 <motion.path variants={{closed: {opacity: 1}, open: {opacity: 0}}}  strokeWidth='3' stroke='black' strokeLinecap="round" d='M 2 9.423 L 20 9.423'/>
-                <motion.path variants={{closed: {d: 'M 2 16.346 L 20 16.346'}, open: {d: 'M 3 2.5 L 17 16.346'}}} strokeWidth='3' stroke='black' strokeLinecap="round"/>
+                <motion.path variants={{closed: {d: 'M 2 16.346 L 20 16.346'}, open: {d: 'M 3 2.5 L 17 16.346'}}} d='M 2 16.346 L 20 16.346' strokeWidth='3' stroke='black' strokeLinecap="round"/>
             </svg>
         </button>
     )
